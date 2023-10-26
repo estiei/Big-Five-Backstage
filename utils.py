41647e2c8fd30bb4819921353b4382df84584b72
Porvatov Vadim <eighonet@gmail.com>
2023-10-26 13:29:55 +0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -47,10 +47,6 @@ def is_correct(dataset:pd.DataFrame, baseline: pd.DataFrame, trait: str, feature
 
 
   return True if (correct_marker == 'M' and mean1 - mean2 > 0) or (correct_marker == 'F' and mean1 - mean2 < 0) else False
-#  if (correct_marker == 'M' and mean1 - mean2 > 0) or (correct_marker == 'F' and mean1 - mean2 < 0):
-#    return True
-#  else:
-#    return False
 
 def d_corresp(baseline: pd.DataFrame, d_fict: dict) -> tuple[list,list,list]:
   '''
